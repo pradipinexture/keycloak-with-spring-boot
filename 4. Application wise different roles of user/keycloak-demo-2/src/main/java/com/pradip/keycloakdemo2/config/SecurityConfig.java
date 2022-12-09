8921ac182601b24d00ac36dfd9c8bc7da5ea2063
pradipinexture <pradip.inexture@gmail.com>
2022-12-09 13:25:55 +0530

    restriction project using client role added

---
+++ 4. Application wise different roles of user/keycloak-demo-2/src/main/java/com/pradip/keycloakdemo2/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.pradip.keycloakdemo2.config;
+
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+@KeycloakConfiguration
+@Import(KeycloakSpringBootConfigResolver.class)
+public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        KeycloakAuthenticationProvider keycloakAuthenticationProvider=keycloakAuthenticationProvider();
+        keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
+        auth.authenticationProvider(keycloakAuthenticationProvider);
+    }
+    @Bean
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(buildSessionRegistry());
+    }
+    @Bean
+    protected SessionRegistry buildSessionRegistry() {
+        return new SessionRegistryImpl();
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/user*").hasRole("user")
+                .antMatchers("/admin*").hasRole("admin")
+                .antMatchers("/superadmin*").hasRole("super-admin")
+                .anyRequest().permitAll()
+                .and().logout().logoutUrl("/logout").logoutSuccessUrl("/")
+                .and().exceptionHandling().accessDeniedPage("/accessdenied");
+        http.csrf().disable();
+    }
+}
--- 4. Application wise different roles of user/readme.md
+++ 4. Application wise different roles of user/readme.md
@@ -1,1 +1,36 @@
-readme
+Application wise different roles of user
+=======================================
+
+
+keycloak 
+--------
+
+1. Create two client in keycloak
+
+	EX : spring-boot-app-1, spring-boot-app-2
+
+2. Create realm roles
+
+	EX: 
+	  - For spring-boot-app-1 client we create 
+	     1. f_user_role, 
+	     2. f_admin_role
+	  - For spring-boot-app-2 client  we create 
+	     1. s_user_role, 
+	     2. s_admin_role and 
+	     3. s_super_admin_role
+
+3. Create user and assign roles
+
+	EX : we create test user and he has a below roles
+		1. f_user_role
+		2. s_admin_role
+		3. s_super_admin_role
+
+4. Now run both spring boot applications
+ 
+5. Test it.
+
+	EX : 
+	- Test user can access user resources and he can not access admin resources of first application.
+	- For second application he can able to access admin and super admin resources and can not access user resource.
