0bb34d9f3a1f129331d00c73fc8a9a3694bedb2c
pradipinexture <pradip.inexture@gmail.com>
2022-12-13 12:49:57 +0530

    added 20.0.3 version of keyclaok and sso multiple project remove and added with UI

--- 5. Multi Tenant/Multi-Tenant-Task (Pradip)/realm-microservice/src/main/java/com/keycloak/realmmicroservice/config/KeycloakConfig.java
+++ 5. Multi Tenant/Multi-Tenant-Task (Pradip)/realm-microservice/src/main/java/com/keycloak/realmmicroservice/config/KeycloakConfig.java
@@ -1,6 +1,7 @@
 package com.keycloak.realmmicroservice.config;
 
 import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.jboss.resteasy.client.jaxrs.internal.ResteasyClientBuilderImpl;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.KeycloakBuilder;
 import org.springframework.context.annotation.Bean;
@@ -35,7 +36,7 @@ public class KeycloakConfig {
                     .password(password)
                     .clientId(clientId)
                     .clientSecret(clientSecret)
-                    .resteasyClient(new ResteasyClientBuilder().connectionPoolSize(10).build())
+                    .resteasyClient(new ResteasyClientBuilderImpl().connectionPoolSize(10).build())
                     .build();
         System.out.println("Keycloak bean created");
         return keycloak;
